541f38704e61137f1a8da7f6a5e255414c3366f0
almohannad <105430204+A1MOHND@users.noreply.github.com>
2025-05-18 00:20:29 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.160.0/build/three.module.js';
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.157.0/build/three.module.js';
 import { OBJLoader } from 'https://cdn.jsdelivr.net/npm/three@0.160.0/examples/jsm/loaders/OBJLoader.js';
 
 // Setup scene, camera, renderer
